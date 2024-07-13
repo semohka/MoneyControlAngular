4b5e60b4122f0e622c48f96077d5f2787c856812
Kristina <semohka@yandex.ru>
2024-07-13 21:44:13 +0300

    Редактирование, удаление магазинов и категорий

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {categories} from '../categories';
+import {categories, Category} from '../categories';
 
 @Component({
   selector: 'app-category',
@@ -12,5 +12,22 @@ export class CategoryComponent {
 
   addCategory(name: string) {
     this.categories.unshift({name: name});
+  }
+
+  editable = false;
+
+  saveCategory(name: string, category: Category) {
+    if (!name) return;
+    this.editable = false;
+    category.name = name;
+  }
+
+  edit() {
+    this.editable = !this.editable
+  }
+
+  remove(category: Category) {
+    let index = this.categories.indexOf(category);
+    this.categories.splice(index, 1)
   }
 }
